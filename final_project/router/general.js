7c590a2c3929d252e7ae4dec98c047dea613b7b9
yuxinens <zendhop@gmail.com>
2025-01-21 10:53:52 -0500

    Task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -55,8 +55,15 @@ public_users.get('/title/:title', function (req, res) {
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
+public_users.get('/review/:isbn', function (req, res) {
+    const { isbn } = req.params;
+    const book = books[isbn];
+  
+    if (book) {
+      return res.status(200).json(book.reviews);
+    } else {
+      return res.status(404).json({ message: "Book not found" });
+    }
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
