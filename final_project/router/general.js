50dfda9e8270a5e182994879e97fa22d4e3cacb5
yuxinens <zendhop@gmail.com>
2025-01-21 10:41:16 -0500

    Task 1 and 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,16 +11,22 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+  return res.status(200).json(books);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+    const { isbn } = req.params;
+    const book = books[isbn];
+  
+    if (book) {
+      return res.status(200).json(book);
+    } else {
+      return res.status(404).json({ message: "Book not found" });
+    }
+    return res.status(300).json({message: "Yet to be implemented"});
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
