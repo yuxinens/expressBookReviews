ba2490980faf30260cca49a650ccf14dd97ee05a
yuxinens <zendhop@gmail.com>
2025-01-21 12:34:26 -0500

    Task 9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -22,7 +22,7 @@ const authenticatedUser = (username, password) => {
     return users.some(user => user.username === username && user.password === password);
 };
 
-//only registered users can login
+// Login
 regd_users.post("/login", (req, res) => {
     const { username, password } = req.body;
 
@@ -46,7 +46,7 @@ regd_users.post("/login", (req, res) => {
 regd_users.put("/auth/review/:isbn", (req, res) => {
     const { isbn } = req.params;
     const { review } = req.body;
-    const username = req.user.username; // Ambil username dari token
+    const username = req.user.username;
 
     if (!books[isbn]) {
         return res.status(404).json({ message: "Book not found" });
@@ -58,6 +58,23 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
 
     books[isbn].reviews[username] = review;
     return res.status(200).json({ message: "Review added/updated successfully" });
+});
+
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const { isbn } = req.params;
+    const username = req.user.username;
+
+    if (!books[isbn]) {
+        return res.status(404).json({ message: "Book not found" });
+    }
+
+    if (books[isbn].reviews && books[isbn].reviews[username]) {
+        delete books[isbn].reviews[username];
+        return res.status(200).json({ message: "Review deleted successfully" });
+    } else {
+        return res.status(404).json({ message: "No review found for this user" });
+    }
 });
 
 module.exports.authenticated = regd_users;
